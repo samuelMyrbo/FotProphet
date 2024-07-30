02ea8a39d0e69cae46edf80d03d5a228eb9668f0
Samuel Myrbostad <samuelmyrbo@gmail.com>
2024-07-30 15:56:25 +0200

    Switch to bcryptjs to avoid native module issues

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 const User = require('../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const adminLayout = '../views/layouts/admin';
